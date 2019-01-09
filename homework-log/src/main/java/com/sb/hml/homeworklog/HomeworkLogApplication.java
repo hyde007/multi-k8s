c8daf296b9108b32a56be60278166397fce24af8
Sharath <sballari@sapient.com>
2019-01-09 15:00:58 +0530

    Added Eureka support

--- homework-log/src/main/java/com/sb/hml/homeworklog/HomeworkLogApplication.java
+++ homework-log/src/main/java/com/sb/hml/homeworklog/HomeworkLogApplication.java
@@ -2,10 +2,12 @@ package com.sb.hml.homeworklog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients(basePackages="com.sb.hml.homeworklog.proxy")
+@EnableEurekaClient
 public class HomeworkLogApplication {
 
 	public static void main(String[] args) {
--- ronswanson-quotes/src/main/java/com/sb/ronswanson/quotes/ronswansonquotes/RonswansonQuotesApplication.java
+++ ronswanson-quotes/src/main/java/com/sb/ronswanson/quotes/ronswansonquotes/RonswansonQuotesApplication.java
@@ -2,8 +2,10 @@ package com.sb.ronswanson.quotes.ronswansonquotes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class RonswansonQuotesApplication {
 
 	public static void main(String[] args) {
